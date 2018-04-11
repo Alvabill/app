eb3668deb1be2a27c9910d9d89d5e78135faf398
Alvabill <1594546640@qq.com>
2018-04-11 16:41:30 +0800

    update getmovie:click and select

--- getMovie/lib/main.js
+++ getMovie/lib/main.js
@@ -18,28 +18,30 @@ router.get('/',function(req,res){
 		if(err)throw err;
 		url_data = data.split('\n').filter(function(n){
 			return n != '';
-		});
+		});//console.log(url_data);
 		var str = '<div style="width:50%;margin:auto;">';
-        str += '<h4 style="padding-left:10px;">(温馨提示：复制ftp开头的路径到‘迅雷极速版’（邮件附件里面有）就会自动下载电影了, 最新免费电影节目单不定时更新，福利呦)</h4>'
-        //console.log(item);
+        str += '<h4 style="padding-left:10px;">(温馨提示：复制ftp开头的路径到‘迅雷极速版’自动下载电影, 最新免费电影节目单不定时更新)</h4>'
         item.forEach(m => {
             str += '<h3 style="padding-left:10px;">' + m.name + '</h3>';
             m.data.forEach((n) => {
                 url_data.forEach(j => {
                     var name = j.split('~~')[0];
-                    name = name.split('.')[0];
+					name = name.split('.')[0];
                     if (n.title.indexOf(name) > -1) {
-                        n.download_url = j.split('~~')[1];
+						n.download_url = j.split('~~')[1];
+						n.download_url = n.download_url.replace(/磁力链下载点击这里/, '');
                     }
-                });
-                str += '<div style="">' +
-                    '<a href="' + n.href + '" style="height:30px;display:inline-block;vertical-align: middle;text-decoration:none;margin-left:6px;" target="_blank">' + n.title + '</a>' +
-                    '<span style="height:30px;display:inline-block;vertical-align: middle;color: red;float:right;">' + n.date + '</span>' +
-                    '</div>';
-                str += '<div style="background:#fdfddf;border:1px solid #ccc;padding:3px 10px;margin-bottom:10px;">' + n.download_url + '</div>';
-            });
+				});
+				if(n.download_url != null){
+					str += '<div style="">' +
+						'<a href="' + n.href + '" style="height:30px;display:inline-block;vertical-align: middle;text-decoration:none;margin-left:6px;" target="_blank">' + n.title + '</a>' +
+						'<span style="height:30px;display:inline-block;vertical-align: middle;color: red;float:right;">' + n.date + '</span>' +
+						'</div>';
+					str += '<div style="background:#fdfddf;border:1px solid #ccc;padding:3px 10px;margin-bottom:10px;">点击全选：<input onClick="this.setSelectionRange(0, this.value.length)" style="border:none; outline:none" value="' + n.download_url + '"></div>';
+				}
+			});
         });
-        str += '</div>';
+		str += '</div>';
         fs.writeFile(path.join(__dirname,'../doc','movies.html'),str,function(){});
         res.send(str);
 	});
